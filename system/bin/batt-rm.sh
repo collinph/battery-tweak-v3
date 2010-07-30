1a2028b52c1d81b0336b181b1363dac6029a1578
Decad3nce <abegovic23@gmail.com>
2010-07-29 20:31:56 -0500

    Added batt-rm.sh for easy removal of Battery Tweak Script

---
+++ system/bin/batt-rm.sh
@@ -0,0 +1,41 @@
+#!/system/bin/sh
+
+#Removal Beta v1.0 by Decad3nce
+mount -o rw,remount /dev/block/mtd3 /system
+#really complex shit eh?
+application="Battery Tweak Script";
+#end of complex shit
+
+RemoveAllFiles()
+{
+   rm /system/bin/batt.sh;
+   rm /system/bin/batt-cfg;
+   rm /system/etc/batt.conf;
+   log "collin_ph: Removed $application";
+   rm /system/bin/BatteryTweakRemover.sh;
+   mount -o remount,ro /
+   exit;
+}
+
+DontRemoveFiles()
+{
+   log "collin_ph: Canceled Removal of $application";
+   mount -o remount,ro /
+   exit;
+}
+
+echo ""
+echo ""
+echo ""
+
+echo "This tool will remove almost all traces of the Battery Tweak"
+echo "Are you sure that you want to go through with this?[y/n]"
+read ANS
+
+case $ANS in
+           "y") RemoveAllFiles;;
+           "n") DontRemoveFiles;;
+             *) echo "collin_ph: Cancelled Removal of $application";;
+esac
+
+done
