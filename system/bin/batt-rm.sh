3d3d24ee385a6e672ad86053709373061cb371f1
Decad3nce <abegovic23@gmail.com>
2010-07-29 23:29:11 -0500

    Had wrong name for batt-rm in RemoveAllFiles and Added mounting in each definition instead of at start of script

--- system/bin/batt-rm.sh
+++ system/bin/batt-rm.sh
@@ -1,24 +1,25 @@
 #!/system/bin/sh
 
 #Removal Beta v1.0 by Decad3nce
-mount -o rw,remount /dev/block/mtd3 /system
 #really complex shit eh?
 application="Battery Tweak Script";
 #end of complex shit
 
 RemoveAllFiles()
 {
+   mount -o rw,remount /dev/block/mtd3 /system
    rm /system/bin/batt.sh;
    rm /system/bin/batt-cfg;
    rm /system/etc/batt.conf;
    log "collin_ph: Removed $application";
-   rm /system/bin/BatteryTweakRemover.sh;
+   rm /system/bin/batt-rm.sh;
    mount -o remount,ro /
    exit;
 }
 
 DontRemoveFiles()
 {
+   mount -o rw,remount /dev/block/mtd3 /system 
    log "collin_ph: Canceled Removal of $application";
    mount -o remount,ro /
    exit;
